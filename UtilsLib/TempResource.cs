3e717b345bc605993551375538a4045f40109759
eight_y_88 <jadec.eight@gmail.com>
2021-10-07 19:41:18 +0900

    Update

--- UtilsLib/TempResource.cs
+++ UtilsLib/TempResource.cs
@@ -1,12 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+
+using System;
+using System.IO;
 
 namespace UtilsLib
 {
-    class TempResource
+    // https://takap-tech.com/entry/2019/02/24/114210
+    public static class TempResource
     {
+        // 使用後に消える一時フォルダを使用するためのコンテキストを提供します。
+        public static void FolderContext(Action<string> f)
+        {
+            string path = string.Empty;
+            try
+            {
+                // 一時フォルダに作業用のサブフォルダを作成
+                path = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+                Directory.CreateDirectory(path);
+
+                f(path);
+            }
+            finally
+            {
+                if (Directory.Exists(path))
+                {
+                    Directory.Delete(path, true);
+                }
+            }
+        }
     }
 }
